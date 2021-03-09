8ca29a1fdc512eba2f6965133b15bd6430de21fb
Leo <efrenn98@gmail.com>
2021-03-09 05:56:29 -0800

    commit tree 99d06b64525eb580a97056a3ae417263188dcdf4

---
+++ app/validators/post_validator.rb
@@ -0,0 +1,13 @@
+   module Validators
+    class PostValidator < ActiveModel::PostValidator
+            def validate(record)
+                if record.title
+                    reqs = ["Won't Believe", "Secret", "Top[number]", "Guess"]
+                    if reqs.detect {|el| record.title.include?(el) }.nil?
+                    record.errors[:title] << "Must contain clickbait"
+                    end
+                end 
+                        
+            end 
+        end 
+    end 
